6264abde961154f0317c0ae8e58b595f77212576
Nopass09 <gal.bogdan2015@gmail.com>
2023-07-27 20:58:06 +0500

    fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -16,6 +16,8 @@ const getBaseUrl = () => {
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
+
+
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
   config() {
